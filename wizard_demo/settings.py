96a50258b3731ba78ab3ac6675ff7c4d12b13f81
bcdharrison <harrisonm@bcdgroup.nz>
2024-05-02 11:38:09 +1200

    sort out secret

--- wizard_demo/settings.py
+++ wizard_demo/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-xrnar-(_$iwu*(%!30=^-v-43**e_92ckm+7=ls96%^&0-8yik"
+#SECRET_KEY = "django-insecure-xrnar-(_$iwu*(%!30=^-v-43**e_92ckm+7=ls96%^&0-8yik"
+SECRET_KEY = os.environ.get('SECRET', "django-insecure-xrnar-(_$iwu*(%!30=^-v-43**e_92ckm+7=ls96%^&0-8yik")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
